a512b84fabf72bd37fe84730e9f2546dd5c200dc
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-17 11:40:29 +0600

    add dropdown structure

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Header.css';
 import { AiOutlineMenu } from "react-icons/ai";
-import logo2 from '../../assets/Images/Logo/logo2.jpg'
+// import logo2 from '../../assets/Images/Logo/logo2.jpg'
+import logo from '../../assets/Images/Logo/logo.jpg'
 
 const Header = () => {
     return (
@@ -10,7 +11,7 @@ const Header = () => {
 
                 <div className="header-main">
                     <div className="logoPart">
-                        <a href="#"><img src={logo2} alt="not available" /></a>
+                        <a href="#"><img src={logo} alt="not available" /></a>
                     </div>
                     <div className="navLinks">
                         <ul className='nav_links'>
@@ -29,12 +30,40 @@ const Header = () => {
                                 </ul>
 
                             </li>
-
-
                             <li><a href="/about">About Me</a></li>
-                            <li><a href="/services">Services</a></li>
-                            <li><a href="/portfolio">Portfolio</a></li>
-                            <li><a href="/blogs">Blogs</a></li>
+                            <li>
+                                <a href="/services">Services</a>
+                                <ul class="drop_menu">
+                                    <li className='menu'>
+                                        <a href="/first">Home Page 01</a>
+                                    </li>
+                                    <li className='menu1'>
+                                        <a href="./manage">Home Page 02 </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/portfolio">Portfolio</a>
+                                <ul class="drop_menu">
+                                    <li className='menu'>
+                                        <a href="/first">Home Page 01</a>
+                                    </li>
+                                    <li className='menu1'>
+                                        <a href="./manage">Home Page 02 </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/blogs">Blogs</a>
+                                <ul class="drop_menu">
+                                    <li className='menu'>
+                                        <a href="/first">Home Page 01</a>
+                                    </li>
+                                    <li className='menu1'>
+                                        <a href="./manage">Home Page 02 </a>
+                                    </li>
+                                </ul>
+                            </li>
                             <li><a href="/contact">Contact</a></li>
 
                         </ul>
