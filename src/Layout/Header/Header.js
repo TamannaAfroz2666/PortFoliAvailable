e5cfb4816660435af3b7b96efe03b24cf04ae269
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-17 09:19:05 +0600

    horizontal line fixed

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -5,30 +5,35 @@ import logo2 from '../../assets/Images/Logo/logo2.jpg'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className="header-main">
-                <div className="logoPart">
-                    <img src={logo2} alt="not available" />
-                </div>
-                <div className="navLinks">
-                    <ul>
-                        <li><a href="#">Home</a><hr className='underline'/>
-                        </li>
-                        <li><a href="/about">About Me</a></li>
-                        <li><a href="/services">Services</a></li>
-                        <li><a href="/portfolio">Portfolio</a></li>
-                        <li><a href="/blogs">Blogs</a></li>
-                        <li><a href="/contact">Contact</a></li>
+        <div className="header">
+            <div className='headerSub'>
 
-                    </ul>
-                </div>
-                <div className="menuLink">
-                    <div className="menuBar">
-                        <AiOutlineMenu  color='white' size={25}/> 
+                <div className="header-main">
+                    <div className="logoPart">
+                        <img src={logo2} alt="not available" />
+                    </div>
+                    <div className="navLinks">
+                        <ul>
+                            <li><a href="#" className='active'>Home</a><hr className='underline' color='yellow'/>
+                            </li>
+                            <li><a href="/about">About Me</a></li>
+                            <li><a href="/services">Services</a></li>
+                            <li><a href="/portfolio">Portfolio</a></li>
+                            <li><a href="/blogs">Blogs</a></li>
+                            <li><a href="/contact">Contact</a></li>
+
+                        </ul>
+                    </div>
+                    <div className="menuLink">
+                        <div className="menuBar">
+                            <AiOutlineMenu color='white' size={25} />
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
+
     );
 };
 
