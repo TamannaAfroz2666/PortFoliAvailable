3c0cc06c8abbe4f777a7fb5d285609cdd085c868
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-18 09:27:36 +0600

    structural design create

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -4,7 +4,23 @@ import './WelcomePage.css';
 const WelcomePage = () => {
     return (
         <div className='welcome'>
-        
+            <div className="pageHead">
+                <div className="infoSection">
+                    <div className="personalInfo">
+                        <p className='sort_title'>Welcome To My World</p>
+                        <h1>Hey, I'm Tamanna! <br /> Creative Front-End Developer</h1>
+                        <p className='descriptionInfo'>
+                        I'm a Front-End Develpoer & I'm very passionate and dedicated to my work. I have acquired the skills and knowledge necessary to make your project a success.
+                        </p>
+                    </div>
+                    <div className="contactInfo">
+                        
+
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 };
