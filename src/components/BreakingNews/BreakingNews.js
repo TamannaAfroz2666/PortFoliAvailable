6ffe62fd3a437657d4e3b159e4e8e698ae452b00
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-18 21:56:43 +0600

    add tittle going line news castle

--- src/components/BreakingNews/BreakingNews.js
+++ src/components/BreakingNews/BreakingNews.js
@@ -7,7 +7,9 @@ const BreakingNews = () => {
    
     return (
         <div>
-            <marquee  behavior="scroll" direction="left"> HTML scrolling text...  2500+ project </marquee>
+            <marquee className='newsTitle' behavior="scroll" direction="left infinite">
+                20 + Satisfied Clients   <span className='starIcon'>*</span> 10 + Team Members  
+                <span className='starIcon'>*</span>     2500+ project Complete   <span className='starIcon'>*</span>  2 Languages  </marquee>
         </div>
     );
 };
