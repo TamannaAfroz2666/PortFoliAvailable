2e775881867420ab8a635251d7cdd94f26e004db
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-18 13:33:40 +0600

    add new animation

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './WelcomePage.css';
+import fish1 from '../../assets/Images/Logo/3d-basketball-essentials.jpg';
+import { AiOutlineRight } from "react-icons/ai";
 
 const WelcomePage = () => {
     return (
@@ -10,12 +12,24 @@ const WelcomePage = () => {
                         <p className='sort_title'>Welcome To My World</p>
                         <h1 className='nameTitle'>Hey, I'm Tamanna! <br /> Creative Front-End Developer</h1>
                         <p className='descriptionInfo'>
-                        I'm a Front-End Develpoer & I'm very passionate and dedicated to my work. I have acquired the skills and knowledge necessary to make your project a success.
+                            I'm a Front-End Develpoer & I'm very passionate and dedicated to my work. I have acquired the skills and knowledge necessary to make your project a success.
                         </p>
+
+                        <div id="frameDiv">
+                            {/* <img id="fish1"  alt="fish" /> */}
+                            <span className='span' id="fish1">enjoy</span>
+
+                            <svg fill="none" stroke="orange" stroke-width="3" stroke-dasharray="5,5">
+                                <path d="M 50 80 C 150 -20 250 180 350 80" />
+
+                            </svg>
+                        </div>
+
+
                     </div>
                     <div className="contactInfo">
                         <div className="backgroundPart">
-                            
+
                         </div>
 
 
