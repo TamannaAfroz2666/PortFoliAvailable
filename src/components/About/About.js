d0bc15ed3fc133609a4bd78f46dac4ebccbaa829
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-18 22:12:55 +0600

    add image based

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './About.css';
+import image from '../../assets/Images/Background/ABOUT.jpg'
+
 
 const About = () => {
     return (
-        <div>
-            about 
+        <div className='aboutAll'>
+            <div className="aboutAllInfo">
+                <div className="imageInfo">
+                    <img src={image} alt="image" />
+
+                </div>
+                <div className="informationAll">
+
+                </div>
+            </div>
+        
             
         </div>
     );
