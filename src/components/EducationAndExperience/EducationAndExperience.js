2c67ea63b60d28d5d6c7a498151fd51edc96940f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-19 22:54:30 +0600

    base create

--- src/components/EducationAndExperience/EducationAndExperience.js
+++ src/components/EducationAndExperience/EducationAndExperience.js
@@ -4,9 +4,25 @@ import './EducationAndExperience.css';
 const EducationAndExperience = () => {
     return (
         <div className='educations'>
-            <h1>hi</h1>
-       
-            
+            <div className="educationHead">
+                <div className="educationAndExperience">
+                    <div className="experienceField">
+                        <h1>Experience</h1>
+                        
+
+                    </div>
+                    <div className="EducationField">
+                        <h1>Education</h1>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
         </div>
     );
 };
