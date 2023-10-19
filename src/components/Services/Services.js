77ea6a76727631fbed439d1d60316e8ad5fb3736
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-19 10:08:57 +0600

    base create on service items

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Services.css';
+import image from '../../assets/Images/Background/Leaf.jpg'
 
 const Services = () => {
     return (
@@ -10,6 +11,28 @@ const Services = () => {
                         <p>Services | Offer</p>
                     </div>
                     <p className='serviceTitle'>My Specializations</p>
+                </div>
+                <div className="serviceContent">
+                    <div className="serviceImage">
+                        <img src={image} alt="loading ...." />
+                    </div>
+                    <div className="serviceContentItems">
+                        <div className="designer">
+                            <hr />
+                            <p>Designer</p>
+                            <p>Illustration Designer </p>
+                        </div>
+                        <div className="designer">
+                            <hr />
+                            <p>Designer</p>
+                            <p>Illustration Designer </p>
+                        </div>
+                        <div className="designer">
+                            <hr />
+                            <p>Designer</p>
+                            <p>Illustration Designer </p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
