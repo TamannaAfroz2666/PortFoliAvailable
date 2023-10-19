b2f5cfb614a4012b6947c177a301eac496c33411
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-19 23:11:59 +0600

    heading designed

--- src/components/EducationAndExperience/EducationAndExperience.js
+++ src/components/EducationAndExperience/EducationAndExperience.js
@@ -7,12 +7,35 @@ const EducationAndExperience = () => {
             <div className="educationHead">
                 <div className="educationAndExperience">
                     <div className="experienceField">
-                        <h1>Experience</h1>
-                        
+                        <h1 className='experienceTitle'>Experience</h1>
+                        <div className="experienceContent">
+                            <div className="listOfExperience">
+                                <div className="year">
+                                    <p>In 2012</p>
+                                </div>
+                                <div className="experienceOfTitle">
+                                    <p>Software Engineer</p>
+                                    <p>UI Head and Manager</p>
+                                </div>
 
+                            </div>
+                        </div>
                     </div>
                     <div className="EducationField">
                         <h1>Education</h1>
+
+                        <div className="experienceContent">
+                            <div className="listOfExperience">
+                                <div className="year">
+                                    <p>In 2012</p>
+                                </div>
+                                <div className="experienceOfTitle">
+                                    <p>Software Engineer</p>
+                                    <p>UI Head and Manager</p>
+                                </div>
+
+                            </div>
+                        </div>
 
                     </div>
 
