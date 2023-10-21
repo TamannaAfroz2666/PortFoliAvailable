68adaee5694da946d7849739a1718dfe31644148
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-21 09:39:01 +0600

    color style create

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -3,16 +3,18 @@ import './Testimonials.css';
 
 const Testimonials = () => {
     return (
-        <div className='testimonial'>
-            <div className="testimonialHead">
-                <div className="testimonialContent">
-                    <div className="ratings">
-                        <h1 className='ratingNumber'>4.8</h1>
-                        <p className='ratingTitle'>Rating</p>
+        <div className="testimonial">
+            <div className='testimonialMain'>
+                <div className="testimonialHead">
+                    <div className="testimonialContent">
+                        <div className="ratings">
+                            <h1 className='ratingNumber'>4.8</h1>
+                            <p className='ratingTitle'>Rating</p>
+                        </div>
                     </div>
                 </div>
+
             </div>
-            
         </div>
     );
 };
