8e79c592bd5e6adc67bd3328251fc48eea768f16
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-23 23:58:32 +0600

    add image

--- src/components/Blogs/Usability/ContactMe/ContactMe.js
+++ src/components/Blogs/Usability/ContactMe/ContactMe.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './ContactMe.css';
-
+import image from '../../../../assets/Images/Background/ContactMe/Contact.jpg'
+import image1 from '../../../../assets/Images/Background/ContactMe/ContactMe.jpg'
 const ContactMe = () => {
     return (
         <div className='contactMe'>
             <div className="contactMeHead">
                 <div className="contactMeTitle">
                     <p className='contactOfTitle'>Contact me</p>
-                    <p></p>
+                    
                 </div>
                 <div className="contactMeBody">
+                    <div className="imagePart">
+                        <img src={image1} alt="" />
+
+                    </div>
+                    <div className="ContactInfoHead">
+                       <p className='infoTitle'> Let's Talk About Ideas </p>
+                    </div>
 
                 </div>
 
