1614a9c0283ebccdc2aeece041485b550066a1e2
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-23 15:26:07 +0600

    fqa page done

--- src/components/FQA/FQA.js
+++ src/components/FQA/FQA.js
@@ -14,33 +14,39 @@ const FQA = () => {
                 <div className="fqaContent">
                     <div className="fqaSection">
                         <h1 className='FqaTitle'>FQA</h1>
+                        <p className='fqaDes'>Frequently asked question(FQA) <br /> pages to find answer</p>
                         <img src={image} alt="" />
 
                     </div>
                     <div className="accordionSection">
-                        <Accordion defaultActiveKey={['0']} alwaysOpen>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                        <Accordion  defaultActiveKey={['0']} alwaysOpen>
+                            <Accordion.Item eventKey="0" className='bodyAcc'>
+                                <Accordion.Header className='titleAccor' >What are project based Rates ?</Accordion.Header>
                                 <Accordion.Body>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum.
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="1">
-                                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                                <Accordion.Header className='q'>How many service we provide ?</Accordion.Header>
                                 <Accordion.Body>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum.
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="2">
+                                <Accordion.Header className='q'>What are your terms and conditions ?</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="3">
+                                <Accordion.Header className='q'>What kind of payment do you accept ?</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
