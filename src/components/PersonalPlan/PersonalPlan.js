8fa90f59b424ef6a16acf636b7eec1f7269cc654
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-23 10:42:10 +0600

    working on planning interface

---
+++ src/components/PersonalPlan/PersonalPlan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PersonalPlan.css';
+
+const PersonalPlan = () => {
+    return (
+        <div className='personalPlan'>
+            
+        </div>
+    );
+};
+
+export default PersonalPlan;
