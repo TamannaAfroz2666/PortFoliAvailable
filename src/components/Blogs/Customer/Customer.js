038f95b3050660275799472e7c847d716a9d4a51
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-23 18:49:20 +0600

    sort description create

--- src/components/Blogs/Customer/Customer.js
+++ src/components/Blogs/Customer/Customer.js
@@ -15,7 +15,7 @@ const Customer = () => {
                         <img className='img' src={image1} alt="" />
                         <p className='customerDate'>Web Design /April 30, 2023</p>
                         <p className='titleCard'>Building brands though customer service </p>
-                        <p> It approach that brands can use to establish familiarity, trust, and added-value for their customers through brand positioning.</p>
+                        <p className='cardDes'> It approach that brands can use to establish familiarity, trust, and added-value for their customers through brand positioning.</p>
 
 
 
