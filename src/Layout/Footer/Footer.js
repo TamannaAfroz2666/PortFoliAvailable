14d9f8790fef7de3cfd8dbb4cbb4746323513836
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-24 10:34:22 +0600

    content list base create

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import image1 from '../../assets/Images/Logo/Footer/Footer1.jpg'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -10,10 +11,38 @@ const Footer = () => {
                     <div className="footerImage">
                         <img src={image1} alt="" />
                     </div>
+                    <div className="contentList">
+                        <ul>
+                            <li>
+                                <Link to="./home"
+                                >Home</Link>
+                            </li>
+                            <li>
+                                <Link to="./aboutme"
+                                >About me</Link>
+                            </li>
+                            <li>
+                                <Link to="./service"
+                                >Services</Link>
+                            </li>
+                            <li>
+                                <Link to="./portfolio"
+                                >Portfolio</Link>
+                            </li>
+                            <li>
+                                <Link to="./blog"
+                                >Blog</Link>
+                            </li>
+                            <li>
+                                <Link to="./Contact"
+                                >Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
 
                 </div>
             </div>
-            
+
         </div>
     );
 };
