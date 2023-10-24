c209d95755b1dc89908b5f916ecacb5caa1e4e30
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-24 09:19:42 +0600

    add googe map

--- src/components/Blogs/Usability/ContactMe/ContactLocation/ContactLocation.js
+++ src/components/Blogs/Usability/ContactMe/ContactLocation/ContactLocation.js
@@ -3,16 +3,40 @@ import './ContactLocation.css';
 import { BsPhoneFlip } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import { TfiLocationPin } from "react-icons/tfi";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+// import GoogleMapReact from 'google-map-react';
+
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
 const ContactLocation = () => {
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    });
+    const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+    // const handleApiLoaded = (map, maps) => {
+    //     // use map and maps objects
+    //     console.log('map');
+    //   };
+
+    // const defaultProps = {
+    //     center: {
+    //       lat: 10.99835602,
+    //       lng: 77.01502627,
+    //       address: '1600 Amphitheatre Parkway, Mountain View, california.'
+    //     },
+    //     zoom: 11
+    // };
     return (
         <div className='contactLocation'>
             <div className="contactLocationBody">
                 <div className="phone">
                     <div className="phoneContents">
                         <div className="phoneIcon">
-                            <BsPhoneFlip size={30} color='white' className='locationPhoneIcon'/>
+                            <BsPhoneFlip size={30} color='white' className='locationPhoneIcon' />
                         </div>
                     </div>
                     <div className="phoneInfo">
@@ -21,9 +45,9 @@ const ContactLocation = () => {
                     </div>
                 </div>
                 <div className="emailContact">
-                <div className="phoneContents">
+                    <div className="phoneContents">
                         <div className="phoneIcon">
-                            <AiOutlineMail size={30} color='white' className='locationPhoneIcon'/>
+                            <AiOutlineMail size={30} color='white' className='locationPhoneIcon' />
                         </div>
                     </div>
                     <div className="phoneInfo">
@@ -33,9 +57,9 @@ const ContactLocation = () => {
 
                 </div>
                 <div className="locationMap">
-                <div className="phoneContents">
+                    <div className="phoneContents">
                         <div className="phoneIcon">
-                            <TfiLocationPin size={30} color='white' className='locationPhoneIcon'/>
+                            <TfiLocationPin size={30} color='white' className='locationPhoneIcon' />
                         </div>
                     </div>
                     <div className="phoneInfo">
@@ -46,7 +70,40 @@ const ContactLocation = () => {
                 </div>
 
             </div>
-            
+
+            {/* <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "" }}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+                >
+                    <AnyReactComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text={defaultProps.center.address}
+                    />
+                </GoogleMapReact>
+            </div> */}
+
+            <div className="mapUsingHead">
+                {!isLoaded ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    <GoogleMap
+                        mapContainerClassName="map-container"
+                        center={center}
+                        zoom={10}
+                    >
+
+                        <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+                    </GoogleMap>
+
+                )}
+
+            </div>
+
         </div>
     );
 };
