c642a6aec65cc5b7e35a8905857f2d2be1ba02ff
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-09 15:25:58 +0600

    add

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,57 +7,58 @@ import Button from '@mui/material/Button';
 
 const Services = () => {
     return (
-        <div className='services'>
-            <div className="servicesHead">
-                <div className="servicesParentNode">
-                    <div className="miniTitle">
-                        <p>Services | Offer</p>
+        <div className="serviceContainer">
+            <div className='services'>
+                <div className="servicesHead">
+                    <div className="servicesParentNode">
+                        <div className="miniTitle">
+                            <p>Services | Offer</p>
+                        </div>
+                        <p className='serviceTitle'>My Specializations</p>
                     </div>
-                    <p className='serviceTitle'>My Specializations</p>
+                    <div className="serviceContent">
+                        <div className="containerImage">
+                            <div className="serviceImage">
+                            </div>
+
+                            <div className=" noteHead">
+
+                                <Button variant="text"> <BsCircleFill className='iconBall' color='yellow' size={20} /></Button>
+                                <Button variant="text"> <BsCircleFill className='iconBallRed' color='yellow' size={10} /></Button>
+                                <h1 className='numberNote'>01</h1>
+                                <p className='noteDescription'>
+                                    There are many variations of passages ipsum available , but the majority  have some form, by injected humour, and randomised words which looks even slightly believable
+                                </p>
+                                <a className='linkMoreNote' href="/readmore"> Read More</a>
+
+                            </div>
+                        </div>
+
+                        <div className="serviceContentItems">
+                            <div className="designer designer animated1 animatedFadeInUp fadeInUp" >
+                                <hr color='black' size={1} className='designerLine' />
+                                <p className='titleDesign'>Designer</p>
+                                <p className='nameOfWork' >Illustration Designer </p>
+                            </div>
+
+                            <div className="designer animated3 animatedFadeInUp fadeInUp">
+                                <hr color='black' size={1} className='designerLine' />
+                                <p className='titleDesign'>UI/UX Design </p>
+                                <p className='nameOfWork'>Web UI/UX Design </p>
+                            </div>
+                            <div className="designer animated4 animatedFadeInUp fadeInUp">
+                                <hr color='black' size={1} className='designerLine' />
+                                <p className='titleDesign'> Web Design </p>
+                                <p className='nameOfWork'>Application Design</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="serviceContent">
-                    <div className="containerImage">
-                        <div className="serviceImage">
-                        </div>
-
-                        <div className=" noteHead">
-
-                            <Button variant="text"> <BsCircleFill className='iconBall' color='yellow' size={20} /></Button>
-                            <Button variant="text"> <BsCircleFill className='iconBallRed' color='yellow' size={10} /></Button>
-                            <h1 className='numberNote'>01</h1>
-                            <p className='noteDescription'>
-                                There are many variations of passages ipsum available , but the majority  have some form, by injected humour, and randomised words which looks even slightly believable
-                            </p>
-                            <a className='linkMoreNote' href="/readmore"> Read More</a>
-
-                        </div>
-                    </div>
-
-                    <div className="serviceContentItems">
-                        <div className="designer designer animated1 animatedFadeInUp fadeInUp" >
-                            <hr color='black' size={1} className='designerLine' />
-                            <p className='titleDesign'>Designer</p>
-                            <p className='nameOfWork' >Illustration Designer </p>
-                        </div>
-                       
-                        <div className="designer animated3 animatedFadeInUp fadeInUp">
-                            <hr color='black' size={1} className='designerLine' />
-                            <p className='titleDesign'>UI/UX Design </p>
-                            <p className='nameOfWork'>Web UI/UX Design </p>
-                        </div>
-                        <div className="designer animated4 animatedFadeInUp fadeInUp">
-                            <hr color='black' size={1} className='designerLine' />
-                            <p className='titleDesign'> Web Design </p>
-                            <p className='nameOfWork'>Application Design</p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
 
-
-
         </div>
+
     );
 };
 
