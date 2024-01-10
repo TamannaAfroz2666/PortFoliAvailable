d6772daf5fb29be48691918481d7647877d9f82f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-10 15:54:04 +0600

    fixed the all issue on home page

--- src/components/Blogs/Customer/Customer.js
+++ src/components/Blogs/Customer/Customer.js
@@ -12,25 +12,15 @@ const Customer = () => {
                 <div className="customerContent">
                     <a href="/customer">
                         <div className="customerCard">
-                        <img className='img' src={image1} alt="" />
-                        <p className='customerDate'>Web Design /April 30, 2023</p>
-                        <p className='titleCard'>Building brands though customer service </p>
-                        <p className='cardDes'> It approach that brands can use to establish familiarity, trust, and added-value for their customers through brand positioning.</p>
-
-
-
-
+                            <img className='img' src={image1} alt="" />
+                            <p className='customerDate'>Web Design /April 30, 2023</p>
+                            <p className='titleCard'>Building brands though customer service </p>
+                            <p className='cardDes'> It approach that brands can use to establish familiarity, trust, and added-value for their customers through brand positioning.</p>
 
                         </div>
-
-                   
-                  
                     </a>
-
                 </div>
-
             </div>
-            
         </div>
     );
 };
