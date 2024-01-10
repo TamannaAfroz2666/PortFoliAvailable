6a833039e70ce77ae3a5bd28b883409e4729fd90
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-10 17:55:54 +0600

    working on side bar

---
+++ src/Layout/SideBar/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideBar.css';
+
+const SideBar = () => {
+    return (
+        <div className='sideBarMain'>
+            
+        </div>
+    );
+};
+
+export default SideBar;
