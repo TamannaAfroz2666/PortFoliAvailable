d94e23f405afb8fcefc692abe0887a61c2c1e7de
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 13:27:25 +0600

    service title header created and styled

--- src/components/ServicePage/ServicePage.js
+++ src/components/ServicePage/ServicePage.js
@@ -4,7 +4,13 @@ import './ServicePage.css';
 const ServicePage = () => {
     return (
         <div className='servicePageTwo'>
-            
+            <div className="servicePageCon">
+                <h1 className='my_service'>my service</h1>
+                <h3 className='passionTitle'>Bringing your vision to life with precision
+                    and passion</h3>
+
+            </div>
+
         </div>
     );
 };
--- src/pages/SideInfo/SideInfo.js
+++ src/pages/SideInfo/SideInfo.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './SideInfo.css'
 import HomePage from './HomePage/HomePage';
 import AboutPageTwo from '../../components/AboutPageTwo/AboutPageTwo';
+import ServicePage from '../../components/ServicePage/ServicePage';
 
 const SideInfo = () => {
     return (
         <div className='sideInfoC1'>
            <HomePage></HomePage>
            <AboutPageTwo></AboutPageTwo>
+           <ServicePage></ServicePage>
 
             
         </div>
