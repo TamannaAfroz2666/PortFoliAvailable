450889d42b90cb6182f4c2dd1ad2dced73dc4224
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 23:57:16 +0600

    fixed the width issue

--- src/components/SkillsPage/SkillsPage.js
+++ src/components/SkillsPage/SkillsPage.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './SkillsPage.css';
+// import './SkillsPage.css';
+import './SkillsPageTwo.css'
 
 const SkillsPage = () => {
     return (
-        <div className='skillsPages'>
-            <div className="skillPageContainer">
-            <h1 className='my_skills'>my skills</h1>
+        <div className='skillsPages11'>
+            <div className="skillPageContainer44">
+                <div className="basicInfo_page">
+                    <h2 className='my_skills44'>my skills</h2>
+                    <div className="des_cri">
+                        <h3 className='skills_header44'>Crafting Stories through Design and
+                            Imagination</h3>
+
+                    </div>
+
+                </div>
+
+
 
             </div>
-          
-            
+
+
         </div>
     );
 };
