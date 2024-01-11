1b52eb9de904beb623649fbc17a3fd12b6134d60
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 11:26:11 +0600

    title add

---
+++ src/components/AboutPageTwo/AboutPageTwo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutPageTwo.css';
+
+const AboutPageTwo = () => {
+    return (
+        <div className='aboutPageTwo'>
+            <div className="aboutCon_pageTwo">
+                <h1 className='title_abPageTwo'>about me</h1>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default AboutPageTwo;
--- src/pages/SideInfo/SideInfo.js
+++ src/pages/SideInfo/SideInfo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SideInfo.css'
 import HomePage from './HomePage/HomePage';
+import AboutPageTwo from '../../components/AboutPageTwo/AboutPageTwo';
 
 const SideInfo = () => {
     return (
         <div className='sideInfoC1'>
            <HomePage></HomePage>
+           <AboutPageTwo></AboutPageTwo>
 
             
         </div>
