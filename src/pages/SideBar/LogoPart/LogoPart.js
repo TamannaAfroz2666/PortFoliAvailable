b1ca26d42908892d9a899c1e4ae9d046753c1897
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 06:36:30 +0600

    logo section done

--- src/pages/SideBar/LogoPart/LogoPart.js
+++ src/pages/SideBar/LogoPart/LogoPart.js
@@ -6,18 +6,18 @@ const LogoPart = () => {
     return (
         <div className='logoPart1'>
             <div className="sideLogoContainer">
-            <div className="logoPartContent1">
-                <div className="logoSide">
-                    <img src={img1} alt="not found" />
+                <div className="logoPartContent1">
+                    <div className="logoSide">
+                        <img src={img1} alt="not found" />
 
+                    </div>
+                    <div className="nameSide">
+                        <p className='name-title'>tamanna</p>
+                    </div>
                 </div>
-                <div className="nameSide">
-                    <p className='name-title'>tamanna</p>
-                </div>
+
             </div>
 
-            </div>
-           
 
         </div>
     );
