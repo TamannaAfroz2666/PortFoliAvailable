2cde1ef08f597f4cdc998906e22b3ff9dbcc3860
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 11:31:52 +0600

    long header add

--- src/components/AboutPageTwo/AboutPageTwo.js
+++ src/components/AboutPageTwo/AboutPageTwo.js
@@ -6,9 +6,11 @@ const AboutPageTwo = () => {
         <div className='aboutPageTwo'>
             <div className="aboutCon_pageTwo">
                 <h1 className='title_abPageTwo'>about me</h1>
+                <h3 className='craftingTitle'>Crafting stories through design and
+                    innovation</h3>
 
             </div>
-            
+
         </div>
     );
 };
