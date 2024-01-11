627c887922b1f3a70c854b08f70319550a733d2c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 10:04:12 +0600

    title animated

---
+++ src/pages/SideInfo/HomePage/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className='home_page'>
+            <div className="home_page_con">
+                <div className="home_page2">
+                    <h1 className='nameTitle_homePage'>
+                        <div class="waviy">
+                            <span style={{ '--i': 1 }}>f</span>
+                            <span style={{ '--i': 2 }}>r</span>
+                            <span style={{ '--i': 3 }}>o</span>
+                            <span style={{ '--i': 4 }}>n</span>
+                            <span style={{ '--i': 5 }}>t</span>
+                            <span style={{ '--i': 6 }}>e</span>
+                            <span style={{ '--i': 7 }}>n</span>
+                            <span style={{ '--i': 8 }}>d</span>
+                            <br />
+                            <strong className='degicnationTitle'>developer</strong>
+                        </div>
+                    </h1>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default HomePage;
--- src/pages/SideInfo/SideInfo.js
+++ src/pages/SideInfo/SideInfo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './SideInfo.css'
+import HomePage from './HomePage/HomePage';
 
 const SideInfo = () => {
     return (
-        <div className='sideInfoC'>
-            <h1>hhh</h1>
+        <div className='sideInfoC1'>
+           <HomePage></HomePage>
 
             
         </div>
