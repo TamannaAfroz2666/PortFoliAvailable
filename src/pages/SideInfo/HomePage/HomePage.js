42a869aec3c177c6445094381897a11b897e02c1
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 10:32:09 +0600

    download cv btn done

--- src/pages/SideInfo/HomePage/HomePage.js
+++ src/pages/SideInfo/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.css';
+import { BiDownArrowAlt } from "react-icons/bi";
 
 const HomePage = () => {
     return (
@@ -22,6 +23,14 @@ const HomePage = () => {
                     </h1>
                     <p className='des_homePage2'>
                         I'm a passionate Front-End Developer with a mission to create delightful and intuitive digital experiences. With a strong foundation in design principles and a keen eye for detail, I specialize in translating complex ideas into user-friendly interfaces that captivate and engage.</p>
+                        <div className="infoCollectCon">
+                            <div className="downloadCv">
+                                <button className='downloadCvBtn'>download cv <span className='spanDoCv'><BiDownArrowAlt size={30}/></span></button>
+                            </div>
+                            <div className="watchVideo">
+
+                            </div>
+                        </div>
                 </div>
 
             </div>
