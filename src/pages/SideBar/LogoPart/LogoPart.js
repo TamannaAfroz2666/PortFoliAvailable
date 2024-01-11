bc6df0b8aa637500fca8cceda458301036702127
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 07:50:46 +0600

    side bar created

--- src/pages/SideBar/LogoPart/LogoPart.js
+++ src/pages/SideBar/LogoPart/LogoPart.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './LogoPart.css';
 import img1 from '../../../assets/Images/Logo/logo.jpg'
 import { IoHomeOutline } from "react-icons/io5";
+import { LiaAddressBook } from "react-icons/lia";
+import { PiShoppingBagOpenThin } from "react-icons/pi";
+import { FaPenNib } from "react-icons/fa";
+import { TiMessages } from "react-icons/ti";
+import { RxGithubLogo } from "react-icons/rx";
+import { PiUserSquare } from "react-icons/pi";
 
 const LogoPart = () => {
     return (
@@ -29,28 +35,40 @@ const LogoPart = () => {
                              <span className='sideNavTitleLi'> Home</span> 
                             </li>
                             <li className='sideNavLi1'>
-                                <span className='sideNavIconTi'>
-                                <IoHomeOutline size={50}/>
+                                <span className='sideNavIconTi1'>
+                                <LiaAddressBook size={50}/>
                                 </span>
-                             <span className='sideNavTitleLi'> Home</span> 
+                             <span className='sideNavTitleLi'> about me</span> 
                             </li>
                             <li className='sideNavLi1'>
-                                <span className='sideNavIconTi'>
-                                <IoHomeOutline size={50}/>
+                                <span className='sideNavIconTi1'>
+                                <PiShoppingBagOpenThin size={50}/>
                                 </span>
-                             <span className='sideNavTitleLi'> Home</span> 
+                             <span className='sideNavTitleLi'>service </span> 
                             </li>
                             <li className='sideNavLi1'>
-                                <span className='sideNavIconTi'>
-                                <IoHomeOutline size={50}/>
+                                <span className='sideNavIconTi1'>
+                                <FaPenNib size={40}/>
                                 </span>
-                             <span className='sideNavTitleLi'> Home</span> 
+                             <span className='sideNavTitleLi'> skills</span> 
                             </li>
                             <li className='sideNavLi1'>
-                                <span className='sideNavIconTi'>
-                                <IoHomeOutline size={50}/>
+                                <span className='sideNavIconTi1'>
+                                <TiMessages size={50}/>
                                 </span>
-                             <span className='sideNavTitleLi'> Home</span> 
+                             <span className='sideNavTitleLi'> portfolio</span> 
+                            </li>
+                            <li className='sideNavLi1'>
+                                <span className='sideNavIconTi1'>
+                                <RxGithubLogo size={50}/>
+                                </span>
+                             <span className='sideNavTitleLi'> blog</span> 
+                            </li>
+                            <li className='sideNavLi1'>
+                                <span className='sideNavIconTi1'>
+                                <PiUserSquare size={50}/>
+                                </span>
+                             <span className='sideNavTitleLi'> contact</span> 
                             </li>
                            
                            
