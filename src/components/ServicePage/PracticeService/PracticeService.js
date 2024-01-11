a0ce7487efc95e7bac6a1c26e3f58656b0bc1652
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 20:33:22 +0600

    fixed the issue

---
+++ src/components/ServicePage/PracticeService/PracticeService.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PracticeService = () => {
+    return (
+        <div>
+            
+
+        </div>
+    );
+};
+
+export default PracticeService;
--- src/components/ServicePage/ServicePage.js
+++ src/components/ServicePage/ServicePage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './ServicePage.css';
 import { CgWebsite } from "react-icons/cg";
+import { ImMobile } from "react-icons/im";
+import { FaRegNoteSticky } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+
 
 const ServicePage = () => {
     return (
@@ -26,29 +30,34 @@ const ServicePage = () => {
                         <div className="web3">
                             <div className="website_logo">
                                 <div className="web_logo_page2">
-                                    <span className='websiteSp'><CgWebsite size={85} color='black' /></span>
+                                    <span className='websiteSp'><ImMobile size={85} color='black' /></span>
                                 </div>
-                                <h3 className='web_des_title'>Website design</h3>
-                                <p className='web_design_des'>Any kind of UI that I could construct. Thus, it can be an inventory, blog, e-commerce, etc.</p> 
+                                <h3 className='web_des_title'>App Development</h3>
+                                <p className='web_design_des'>Any kind of UI that I could construct. Thus, it can be an inventory, blog, e-commerce, etc.</p>
                             </div>
                         </div>
                         <div className="web4">
                             <div className="website_logo">
                                 <div className="web_logo_page2">
-                                    <span className='websiteSp'><CgWebsite size={85} color='black' /></span>
+                                    <span className='websiteSp'><FaRegNoteSticky size={85} color='black' /></span>
                                 </div>
-                                <h3 className='web_des_title'>Website design</h3>
-                                <p className='web_design_des'>Any kind of UI that I could construct. Thus, it can be an inventory, blog, e-commerce, etc.</p>
+                                <h3 className='web_des_title'>software testing</h3>
+                                <p className='web_design_des'>Any kind of UI that I could testing on my way. Thus, it can be an inventory, blog, e-commerce, etc.</p>
                             </div>
-
+                            <div className="web_btn_group">
+                                <a href="/read-more">Read more</a>
+                            </div>
                         </div>
-
-                        {/* <div className="web_btn_group">
-                            <a href="/read more">Read more</a>
-                        </div> */}
-
                     </div>
+
+
+
+
+
+
                 </div>
+
+
             </div>
 
         </div>
