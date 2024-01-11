3145a9ba71c0f9a5b88fbf015c0ae44cb381e558
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 13:19:01 +0600

    working on service page

---
+++ src/components/ServicePage/ServicePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ServicePage.css';
+
+const ServicePage = () => {
+    return (
+        <div className='servicePageTwo'>
+            
+        </div>
+    );
+};
+
+export default ServicePage;
