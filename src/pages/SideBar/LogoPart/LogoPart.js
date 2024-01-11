c55bdc36e7b2ba290b691c09e35f134eedb1ecd4
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-11 07:25:15 +0600

    title animated

--- src/pages/SideBar/LogoPart/LogoPart.js
+++ src/pages/SideBar/LogoPart/LogoPart.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './LogoPart.css';
 import img1 from '../../../assets/Images/Logo/logo.jpg'
+import { IoHomeOutline } from "react-icons/io5";
 
 const LogoPart = () => {
     return (
         <div className='logoPart1'>
             <div className="sideLogoContainer">
+                {/* logo part  */}
                 <div className="logoPartContent1">
                     <div className="logoSide">
                         <img src={img1} alt="not found" />
@@ -13,6 +15,32 @@ const LogoPart = () => {
                     </div>
                     <div className="nameSide">
                         <p className='name-title'>tamanna</p>
+                    </div>
+                </div>
+                {/* sidebar add  */}
+                {/* <h1 className='oo'>ooo</h1> */}
+                <div className="sideNavBar">
+                    <div className="sideNavCon">
+                        <ul className='sideNavUi'>
+                            <li className='sideNavLi'>
+                                <span className='sideNavIconTi'>
+                                <IoHomeOutline size={50}/>
+                                </span>
+                             <span className='sideNavTitleLi'> Home</span> 
+                            </li>
+                            <li>
+                            <IoHomeOutline size={40}/> Home
+                            </li>
+                            <li>
+                            <IoHomeOutline size={40}/> Home
+                            </li>
+                            <li>
+                            <IoHomeOutline size={40}/> Home
+                            </li>
+                            <li>
+                            <IoHomeOutline size={40}/> Home
+                            </li>
+                        </ul>
                     </div>
                 </div>
 
