d19f6c3bd43660ac53027706e40d5f17874b0d0f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 12:21:02 +0600

    working on portfolio

---
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PorrtfolioPage.css';
+
+const PortfolioPage = () => {
+    return (
+        <div className='portfolioPage'>
+            
+            
+        </div>
+    );
+};
+
+export default PortfolioPage;
