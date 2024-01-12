99ae0977ceab542b298fee554b92206ba84aad1b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 17:13:21 +0600

    phone device done

--- src/pages/ContactPage/ContactPageBasic.js
+++ src/pages/ContactPage/ContactPageBasic.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ContactPageBasic.css';
 import { TbDeviceMobileVibration } from "react-icons/tb";
+import { CiMobile3 } from "react-icons/ci";
+import { MdOutlineSendToMobile } from "react-icons/md";
 
 const ContactPageBasic = () => {
     return (
@@ -8,13 +10,13 @@ const ContactPageBasic = () => {
             <div className="contact_contents">
                 <div className="phone_multiContent">
                     <div className="items_info">
-                        <TbDeviceMobileVibration />
-                        <h3 className='phone_contactPage'>phone</h3>
+                        <MdOutlineSendToMobile size={85} className='mobileDevice' />
+                        <h1 className='phone_contactPage'>phone</h1>
                         <p className='call_meTitle'>Call me 8.00am - 10.00pm</p>
                         <h3 className='number_show'>+008 13-189-178-74</h3>
 
                     </div>
-                    {/* <h1>phone</h1> */}
+
 
 
                 </div>
