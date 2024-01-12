1ad91586bd79ddacda70e7b9f60251317cc2c429
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 10:15:39 +0600

    base create

--- src/pages/DownloadCv/DownloadCv.js
+++ src/pages/DownloadCv/DownloadCv.js
@@ -26,6 +26,23 @@ const DownloadCv = () => {
 
                 </div>
                 <div className="web_designerCv">
+                <div className="link_creative">
+                        <div className="arrow_cv">
+                            <MdKeyboardDoubleArrowRight className='doubleArrow' size={26} />
+                        </div>
+                        <div className="informationOfCv">
+                            <h4 className='agencyTitle'>Creative agency
+
+                            </h4>
+                            <hr className='hrAgency' />
+                            <h3 className='framerDownTitle'>Framer Designer & Developer</h3>
+                            <h5 className='timeOfFramer'>2019 - Present</h5>
+                            <p className='desWork_down'>This work is mainly performed contractually. here, complete several projects.</p>
+                            <progress className='progress-bar' id="file" max="100" value="20">70%</progress>
+
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
