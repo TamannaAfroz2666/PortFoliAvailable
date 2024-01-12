3ae6173fecd6d0310482860a1630f2546744b82f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 17:53:26 +0600

    update location field

--- src/pages/ContactPage/ContactPageBasic.js
+++ src/pages/ContactPage/ContactPageBasic.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './ContactPageBasic.css';
-import { TbDeviceMobileVibration } from "react-icons/tb";
-import { CiMobile3 } from "react-icons/ci";
+import { MdOutlineBookmarkAdded } from "react-icons/md";
+import { GrMapLocation } from "react-icons/gr";
 import { MdOutlineSendToMobile } from "react-icons/md";
+import { LuMap } from "react-icons/lu";
 
 const ContactPageBasic = () => {
     return (
@@ -19,21 +20,20 @@ const ContactPageBasic = () => {
                 <div className="location_multiContent">
                 <div className="phone_multiContent">
                     <div className="items_info">
-                        <MdOutlineSendToMobile size={85} className='mobileDevice' />
-                        <h1 className='phone_contactPage'>phone</h1>
-                        <p className='call_meTitle'>Call me 8.00am - 10.00pm</p>
-                        <h3 className='number_show'>+008 13-189-178-74</h3>
+                        <LuMap size={85} className='mobileDevice' />
+                        <h1 className='phone_contactPage'>location</h1>
+                        <p className='call_meTitle'>110/9 Kohinoor Mahal,Mohammadpur, Dhaka</p>
+                       <a href="/0131899"> <h3 className='number_show'>View on map</h3></a>
                     </div>
                 </div>
-
                 </div>
                 <div className="day_multiContent">
                 <div className="phone_multiContent">
                     <div className="items_info">
-                        <MdOutlineSendToMobile size={85} className='mobileDevice' />
-                        <h1 className='phone_contactPage'>phone</h1>
-                        <p className='call_meTitle'>Call me 8.00am - 10.00pm</p>
-                        <h3 className='number_show'>+008 13-189-178-74</h3>
+                        <MdOutlineBookmarkAdded size={85} className='mobileDevice' />
+                        <h1 className='phone_contactPage'>Monday-Sunday</h1>
+                        <p className='call_meTitle'>All days have ready for work</p>
+                        <a href="/afroz"><h3  className='email_show'>afroztamanna66@gmail.com</h3></a>
                     </div>
                 </div>
 
