8de7e8c5682da7a656d03426bf9d23c35ef9e51f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 18:44:59 +0600

    input field done

--- src/pages/ContactPage/MessagePage/MessagePage.js
+++ src/pages/ContactPage/MessagePage/MessagePage.js
@@ -10,12 +10,18 @@ const MessagePage = () => {
                         <input type="text" name='name' className='name_fieldMess' id='name_fieldMess' placeholder='Your Name Here' />
                     </div>
                     <div className="sub_message">
-                    <input type="text" name='name' className='name_fieldMess' id='name_fieldMess' placeholder='Subject' />
+                        <input type="text" name='name' className='name_fieldMess' id='name_fieldMess' placeholder='Subject' />
                     </div>
+                </div>
+                <div className="email_message">
+                    <input type="email" name='name' className='email_fieldMess' id='name_fieldMess' placeholder='Your Name Here' />
+                </div>
+                <div className="customer_message">
+                    <textarea type="text" name='name' className='client_fieldMess' id='name_fieldMess' placeholder='Your Message Here' />
                 </div>
 
             </form>
-            
+
         </div>
     );
 };
