3a6a75e378055bcccaa86aadabcf20c50977939e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-12 09:12:39 +0600

    working at cv area

---
+++ src/pages/DownloadCv/DownloadCv.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './DownloadCv.css';
+
+const DownloadCv = () => {
+    return (
+        <div className='downloadCv'>
+            <div className="downloadCvContent">
+                <div className="formerDesigner">
+
+                </div>
+                <div className="web_designerCv">
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default DownloadCv;
